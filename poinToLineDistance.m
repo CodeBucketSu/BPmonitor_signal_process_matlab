ab77f6664ae522e1e7bfc47a949d0e22d7eaa176
henryalps <henryalps@gmail.com>
2015-01-27 10:40:03 +0800

    修正了Git使用不熟练引发的未同步问题

--- poinToLineDistance.m
+++ poinToLineDistance.m
@@ -1,4 +1,4 @@
-function [distances,maxpos] = poinToLineDistance(points,pointOnLine1,pointOnLine2)
+function [distances,maxpos,pointAndLineRelation] = poinToLineDistance(points,pointOnLine1,pointOnLine2)
 	%% Caculate the distance between each point in [points] and a line,
 	%	which conclude points {pointOnLine1}{pointOnLine2}.
 	%%	INPUT
@@ -7,6 +7,9 @@ function [distances,maxpos] = poinToLineDistance(points,pointOnLine1,pointOnLine
 	%%	OUTPUT
 	%	distances:[N,1]row vector Distance array between points outside the line and the line.
 	%	maxpos: [1,1]the max value's pos in the distance vector
+    %   pointAndLineRelation:
+    %   [1,1]如果points中只包含一个点，那么该变量返回该点与直线的位置关系。1-点处于直线上方 0-点位于直线上
+    %   -1-点处于直线下方
 	
 	%% STEP 1:Make sure the line's exist
 	pointOnLine1 = (pointOnLine1(:))';
@@ -17,10 +20,10 @@ function [distances,maxpos] = poinToLineDistance(points,pointOnLine1,pointOnLine
 	end
 
 	%% STEP 2:Preprocess
-	if length(points)==2 && length(points')~=2
+	if size(points) == [2,1]
 		points = points';
 	end
-	len = length(points);
+	len = length(points(:,1));
 	q1 = ones(len,1)*pointOnLine1;
 	q2 = ones(len,1)*pointOnLine2;
 
@@ -32,3 +35,13 @@ function [distances,maxpos] = poinToLineDistance(points,pointOnLine1,pointOnLine
 	distances = sqrt(d(:,1).^2 + d(:,2).^2);
 	[~, maxpos] = max(distances);
 
+    %% STEP 5:如果只有一个点，则确定其与直线的关系
+    if len ==1
+        if d(1,2)>0
+            pointAndLineRelation = 1;
+        elseif d(1,2)==0
+            pointAndLineRelation = 0;
+        else
+            pointAndLineRelation = 1;
+        end
+    end
