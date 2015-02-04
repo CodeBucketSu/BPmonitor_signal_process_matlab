24c7b98f9dd613da9cb45a85d0c9f5b22e36b94f
Toilet22 <fe22suyilun@gmail.com>
2015-02-04 09:37:30 +0800

    MOD: detectNotch.

--- computeAllFeaturesFromOneMeasureEvent.m
+++ computeAllFeaturesFromOneMeasureEvent.m
@@ -47,3 +47,12 @@ if (needPlot || hasError)
 end
 
 end
+
+
+function figures = recordFeatureIfNeeded(figs, fig, ifNeeded)
+    if ifNeeded
+        figures = [figs(:); fig];
+    else
+        figures = figs;
+    end
+end
