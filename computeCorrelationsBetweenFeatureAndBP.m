72d6547696b3e019c200fa8bfea090357c23bbee
Toilet22 <fe22suyilun@gmail.com>
2015-02-06 20:57:58 +0800

    FIX: NaN bug.  ADD: chooseMethod.

--- computeCorrelationsBetweenFeatureAndBP.m
+++ computeCorrelationsBetweenFeatureAndBP.m
@@ -1,4 +1,4 @@
-    function [corrBP2Feature, corrFeature2HR, corrBP2HR, fig] = computeCorrelationsBetweenFeatureAndBP(bp, features, featureNames, hr, corrFeatureHrs, titleOfSignals)
+function [corrBP2Feature, corrFeature2HR, corrBP2HR, fig] = computeCorrelationsBetweenFeatureAndBP(bp, features, featureNames, hr, corrFeatureHrs, titleOfSignals)
 
 bpNomal = (bp - mean(bp)) ./ std(bp);
 hrNomal = (hr - mean(hr)) ./ std(hr);
@@ -9,20 +9,25 @@ len = size(features, 1);
 corrBP2Feature = zeros(len, 2);
 corrBP2HR = zeros(len, 2);
 corrFeature2HR = zeros(len, 2);
-
+bp1 = bp;
+hr1 = hr;
 
 [row, col] = getSubplotsSize(len);
 
 for i = 1 : len
     %% 准备工作
     feature = features(i, :);
-    feature = feature(~isnan(feature));
+    idxNotNaN = ~isnan(feature);
+    feature = feature(idxNotNaN);
+    bp = bp1(idxNotNaN);
+    hr = hr1(idxNotNaN);
     name = featureNames{i};
     
     %% 步骤1：去基线，归一化
     featureNormal = (feature - mean(feature)) ./ std(feature);
     
     %% 步骤2：找出对应点,并计算相关性
+    
     [corrBP2Feature(i, 1), corrBP2Feature(i, 2)] = corr(bp.', feature.');
     [corrBP2HR(i, 1), corrBP2HR(i, 2)] = corr(bp.', hr.');
     [corrFeature2HR(i, 1), corrFeature2HR(i, 2)] = corr(feature.', hr.');
