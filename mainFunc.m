ffd5a85bb910d60385d89c57f6ec7a6164975c66
henryalps <henryalps@gmail.com>
2015-02-06 21:37:31 +0800

    增加了mainFunc方法
    Signed-off-by: henryalps <henryalps@gmail.com>

--- mainFunc.m
+++ mainFunc.m
@@ -1,21 +1,36 @@
-function [ pwttcorr,pwfcorrelb,pwfcorrwrt ] = mainFunc( directory,method )
+function [ pwttcorr,pwfcorrelb,pwfcorrwrt ] = mainFunc( method,dataType,path )
 %MAINFUNC 代替原来的main函数在批处理中调用，每次获取一种方法对应的3个相关矩阵
 %   输入
-%   directory 字符串类型 数据所在的processed目录
 %   method 字符串类型 "PEAK" "DISTANCE" "WAVELET" 采取的方法 
+%   dataType 字符串类型 "CALIBRITION" "EXERCISE" "STATIC"
+%   path 字符串类型 数据所在的processed目录
+
+%%预定义
+needPlot = 0;
+save('method.mat','method');
+
+if strcmp(dataType,'CALIBRITION')
+    candidates = {'pf','xc','sj'};
+    fileNameforSave = '/result_cali.mat';
+elseif strcmp(dataType,'EXERCISE')
+    candidates = {'ydh'};
+    fileNameforSave = '/result_exec.mat';
+else
+    candidates = {'static'};
+    fileNameforSave = '/result_stat.mat';
+end
 
 %% 选取数据所在文件夹
-filepath = directory;
-filePathForDate = [filePath, '/data'];
-filePathForSave = [filePath, '/result'];
+filePathForData = [path, '/data'];
+filePathForSave = [path, '/result'];
 
-%% 处理运动前的信号
-fileNames = {'pf.mat', 'xc.mat' };  %'sj.mat', 'xc.mat','pf.mat', 'ydh.mat'
+%% 处理信号
+fileNames = getFileNamesforBatch(candidates,filePathForData); 
 [HRs, BPs, PWTTs, PWFs_elb, PWFs_wrst, PWFnames, PWTTstats, PWFstats_elb, PWFstat_wrst, corrBpHr, figures]...
-    = computeAll(filePathForDate, fileNames, needPlot, '标定数据');
+    = computeAll(filePathForData, fileNames, needPlot, '标定数据');
 saveFigures(figures, filePathForSave, ...
     {'标定数据：PWTT与BP的相关性'; '标定数据：PWF_elbw与BP的相关性';  '标定数据：PWF_wrst与BP的相关性'});
 
-save([filePathForSave, '/result.mat']);
+save([filePathForSave, fileNameforSave]);
 end
 
